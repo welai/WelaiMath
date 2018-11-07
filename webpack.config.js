e0c07304a3f5ff8c94d5c7ee40804869d99aeb55
celestialphineas <hanghang0713@hotmail.com>
2018-11-07 10:42:10 +0800

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+function generateConfig(name) {
+  var uglify = name.indexOf('min') > -1;
+  var config = {
+    mode:   'development',
+    entry:  './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: name + '.js',
+      sourceMapFilename: name + '.map',
+      library: 'gridcanvas',
+      libraryTarget: 'umd'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    }
+  };
+  config.optimization = {};
+  if(uglify) {
+    config.optimization.minimize = true;
+    config.mode = 'production';
+  }
+  return config;
+}
+
+module.exports = ['welai-math', 'welai-math.min'].map((name) => generateConfig(name));
+
